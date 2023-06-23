37de214b506a76d1a1b10e9c28c4ebe509f87a61
Yu Zhai <me@zhaiyusci.net>
2023-06-23 14:09:47 +0800

    pub reformat

--- js/publistgen.js
+++ js/publistgen.js
@@ -1,4 +1,4 @@
-
+/*
 function generate_pub(){
   let lst = document.getElementById("publist");
 
@@ -31,9 +31,6 @@ function generate_pub(){
         head.append(h4);
         yearcard.append(head);
 
-
-
-
         let collapse = document.createElement("div");
         collapse.id = "collapse"+member.year;
         collapse.className = "panel-collapse collapse";
@@ -46,6 +43,50 @@ function generate_pub(){
         lst = document.createElement("ul");
         document.getElementById("collapse"+member.year).append(lst);
 
+        continue;
+      }
+
+      let div = document.createElement('li');
+      // div.className = "media";
+      let innerhtml = '';
+      innerhtml += member["author"].replace('李辉', '<b>李辉</b>').replace('Hui Li', '<b>Hui Li</b>') + "<br/>";
+      innerhtml += member["title"] + "<br/>";
+      innerhtml += "<i>"+member["journal"]+ "</i>" + " <b>" + member["vol"]+ "</b>, " + member['page'] + " ("+ member['year'] + '). <a href="https://doi.org/' +member['doi']+ '">doi:' + member['doi'] + '</a> ';
+      if (member["comment"] != ""){
+        innerhtml += member['comment'];
+      }
+
+      div.innerHTML = innerhtml;
+      lst.append(div);
+    }
+  }, 'Text');
+
+}
+
+*/
+
+function generate_pub(){
+  let lst = document.getElementById("publist");
+
+  let members;
+  let yearcard;
+  $.get("../data/pub.csv", function (csv) {
+    // console.log(json); // this will show the info it in firebug console
+    members=$.csv.toObjects(csv);
+    for (let member of members) {
+      if(member.author == "*"){
+        head = document.createElement("h1");
+        head.innerHTML = member.year + '<a name="year' + member.year + '">' + '</a>';
+        $("#publist").append(head);
+        li = document.createElement("li");
+        li.setAttribute("role", "presentation");
+        li.innerHTML = '<a href="#year' + member.year + '">' + member.year + '</a>';
+        // document.getElementById("#yearnav").insertBefore(li,document.getElementById("talk"));
+        // $("#yearnav").insertBefore(li, $("talk"));
+        document.getElementById("yearnav").insertBefore(li, document.getElementById("talknav"));
+        lst = document.createElement("ul");
+        // document.getElementById("collapse"+member.year).append(lst);
+        $("#publist").append(lst);
         continue;
       }
 
